96aa2a342515f26ac702bf64eba20f3a84cb2930
pd84jow <pd84jow@eduvaud.ch>
2026-02-05 15:31:13 +0100

    chore(space age):Exercice terminé [20] [DONE]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, seconds) => {
+  const EARTH_YEAR_IN_SECONDS = 31557600;
+  const PLANET_ORBITAL_PERIODS = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  };
+
+  if (planet in PLANET_ORBITAL_PERIODS) {
+    return Number((seconds / (EARTH_YEAR_IN_SECONDS * PLANET_ORBITAL_PERIODS[planet])).toFixed(2));
+  } else {
+    throw new Error('not a planet');
+  }
 };
